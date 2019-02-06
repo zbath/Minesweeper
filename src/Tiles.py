c9b6c8bff600ef4ea1b9ac95880bb0bde3d60a07
Taylor Bauer <taylorbauer@ku.edu>
2019-02-06 10:52:55 -0600

    Added some functionality to Gameboard and fixed member variable names in Tiles

--- src/Tiles.py
+++ src/Tiles.py
@@ -3,31 +3,35 @@ import pygame
 class Tile(object):
     # the _pos variables hold where on the board each tile lives
     # they start at 0
-    x_pos = 0
-    y_pos = 0
+    m_x_pos = 0
+    m_y_pos = 0
 
     # mine is True if the tile contains a mine, False otherwise
     # this will be determined at initilization
-    is_mine = False
-    is_revealed = False
+    m_is_mine = False
+    m_is_revealed = False
 
     # num_mines contains how many mines are adjacent to this tile
     # this will have to be done after the ENTIRE board is generated
-    num_mines = 0
+    m_num_mines = 0
 
-    def __init__(self, x_pos, y_pos, is_mine):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.is_mine = is_mine
+    def __init__(self, x_pos, y_pos):
+        self.m_x_pos = x_pos
+        self.m_y_pos = y_pos
 
     def get_is_revealed(self):
-        return (self.is_revealed)
+        return (self.m_is_revealed)
 
     def get_is_mine(self):
-        return(self.is_mine)
+        return(self.m_is_mine)
 
     def get_num_mines(self):
-        return(self.num_mines)
+        return(self.m_num_mines)
 
+    # accepts num_mines after it is calculated by Gameboard
+    def set_num_mines(self, num_mines):
+        self.m_num_mines = num_mines
+        return (0)
+    
 
-
+        
