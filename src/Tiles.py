91a8e6fd3231690271fe9c4d17101c1861e6878f
Marcus Leong <mwleong25@gmail.com>
2019-02-08 09:24:51 -0600

    Minor indent changes

--- src/Tiles.py
+++ src/Tiles.py
@@ -1,9 +1,14 @@
 #This is the tile class go for the game
 import pygame
 from random import randint
+from src.Styles import Styles
 
 class Tiles:
-    def __init__(self, is_revealed, is_flag, is_mine, display): 
+    # Constructor initializing a Tile object
+    # Tile object will be set to self, booleans(is_revealed, is_flag, is_mine)
+    # Tile object will have an x_pos and y_pos initialized
+    # Display will be called to draw a tile on the board
+    def __init__(self, is_revealed, is_flag, is_mine, x_pos, y_pos, display): 
         self.is_revealed = is_revealed
         self.is_flag = is_flag
         self.is_mine = is_mine
@@ -11,6 +16,8 @@ class Tiles:
         #self.x_pos = Styles[x][y]
         #self.y_pos = Styles[x][y]
 
+    # Given x_pos and y_pos, a tile will call styles to be drawn and displayed
+    # at the coordinate point, scaled to size
     def draw(self, x_pos, y_pos):
         pygame.draw.rect(
             self.display,
@@ -21,16 +28,8 @@ class Tiles:
             ]
         )
 
-    def tile_click():
-        #calls tile_reveal, returns the state
-		if not is_revealed:
-			#call tile_reveal
-			tile_reveal()
-
-		#return state
-		return self
-
-
+    # When called, tile_reveal will display either mine, blank, or number
+    # Returns a boolean which either determines win or loss of a game
     def tile_reveal():
         #if(is_mine):
             #return display
@@ -41,11 +40,14 @@ class Tiles:
             #return boolean to board
             #is_revealed = false
         #if(not is_flag and not is_mine):
+        #TO BE HANDLED IN GAMEBOARD
             #check if is_adjacent
             #recurse as needed
             #return display either numerical or blank
         
-
+    # When called, boolean is_flag will be set to true
+    # Displays a flag on the tile
+    # If is_mine is true, adds to the count
     def tile_flag():
         count = 0
         if is_mine == True:
@@ -61,8 +63,19 @@ class Tiles:
         #otherwise, just add to counter if it IS a mine
         #every click displays flag
 
+    #sets is_mine to true for the Tile object
     def set_mine():
-        #sets true to is_mine
+        is_mine = True
 
+    #returns the truth value of is_mine
     def get_mine():
-        #returns the truth value of is_mine
+        return is_mine
+
+    def tile_click():
+        #calls tile_reveal, returns the state
+		if not is_revealed:
+			#call tile_reveal
+			tile_reveal()
+
+		#return state
+		return self
