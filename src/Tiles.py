f10202cffd74520256916dff88270106999190b2
MeganaChinala <m.chinalachaiagari@gmail.com>
2019-02-11 16:45:18 -0600

    fixed small syntax error within tile_flag

--- src/Tiles.py
+++ src/Tiles.py
@@ -33,7 +33,7 @@ class Tiles:
     def tile_flag(self):
         #user_mines_found tracks how many tiles are CORRECTLY flagged
         users_mines_found = 0
-        if Tile.is_mine == True
+        if (Tile.is_mine == True):
             #if tile clicked is mine, increment user_mines_found by 1
             users_mines_found = users_mines_found + 1
         #return display
