e8c798323a076c4f3b8be5e8191e9ea0ea433913
Myra Dotzel <myradotzel@gmail.com>
2019-02-10 19:45:25 -0600

    modified main so it interacts with UI instead of Game

--- Minesweep.py
+++ Minesweep.py
@@ -1,14 +1,17 @@
 #This is the main executable of the program, it runs Game
 import pygame
+from pygame.locals import*
 
-from src.Game import Game
+from src.UI import UI
+
+pygame.init()
 
 def main():
     display = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption('Play Minesweeper!')
 
-    game = Game(display)
-    game.loop()
+    user = UI(display)
+    user.start_game()
 
 if __name__ == '__main__':
     main()
