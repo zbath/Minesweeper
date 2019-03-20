cab7114f6f8c294f8d0f6d148f685f4a2112c4bd
Miller Bath <millerzbath@gmail.com>
2019-03-19 20:09:53 -0500

    Added all comments for the functions in Tiles.

--- src/Tiles.py
+++ src/Tiles.py
@@ -16,13 +16,11 @@ class Tiles:
     # Display will be called to draw a tile on the board
     def __init__(self, i, j, is_revealed, is_flag, is_mine, display):
         """
-        @pre Initialization of a tile object
-        @param
-            is_revealed: takes a bool if tile object is already revealed
-            is_flag: takes a bool if tile object is flagged
-            is_mine: takes a bool if tile object is a mine
-        @post Initializes display to display, surf to a surface size of 30x30 pixels, then fills each surface with a gray color"
-        @return None
+        Initialization of a tile object
+        is_revealed: takes a bool if tile object is already revealed
+        is_flag: takes a bool if tile object is flagged
+        is_mine: takes a bool if tile object is a mine
+        Initializes display to display, surf to a surface size of 30x30 pixels, then fills each surface with a gray color"
         """
         self.is_revealed = is_revealed
         self.i = i
@@ -38,10 +36,8 @@ class Tiles:
 
     def draw_self(self):
         """
-        @pre Called from update_board in Gameboard.py
-        @param
-        @post Redraws the tile onto the board when doing an update to the gameboard, after a user action.
-        @return
+        Called from update_board in Gameboard.py
+        Redraws the tile onto the board when doing an update to the gameboard, after a user action.
         """
         if self.is_flag:
             self.display.blit(flag_image, self.Rect)
@@ -60,10 +56,8 @@ class Tiles:
 
     def get_coords(self):
         """
-        @pre Grabs the coordinate of a tile that has been specified in boardGame[i][j]
-        @param
-        @post
-        @return Coordinates of the tile on the board
+        Grabs the coordinate of a tile that has been specified in boardGame[i][j]
+        Coordinates of the tile on the board
         """
         coords = (self.i, self.j)
         return coords
@@ -72,22 +66,18 @@ class Tiles:
     def tile_reveal(self):
         """
         Displays the number of mines surrounding a revealed tile (if any exist)
+        When user clicks and the specific tile(or area of recursively revealed tiles) it will change the tile to be revealed
+        Shows the tile on the screen, changes tile objects value to true
 
-        @pre When user clicks and the specific tile(or area of recursively revealed tiles) it will change the tile to be revealed
-        @param
-        @post Shows the tile on the screen, changes tile objects value to true
-        @return
         """
         self.is_revealed = True
 
     def tile_flag(self):
         """
         Adds or removes flag image on tile
-
-        @pre Expects a call from a user right-click
-        @param None
-        @post Flag is displayed or removed from tile
-        @return Returns an integer that will add or subtract from the flag count that is being compared to the mine count
+        Expects a call from a user right-click
+        Flag is displayed or removed from tile
+        Returns an integer that will add or subtract from the flag count that is being compared to the mine count
         """
         if not self.is_revealed:
             if not self.is_flag:
@@ -107,21 +97,15 @@ class Tiles:
 
     def refill(self):
         """
-        @pre After tile is redrawn on the board it is filled
-        @param
-
-        @post
-        @return
+        After tile is redrawn on the board it is filled
         """
         self.org_color=self.hover_color
         self.isHoverbool = True
     # Draws number of adjacent mines to screen
     def isHover(self):
         """
-        @pre If user is looking at the tile
-        @param
-        @post Switch the values of the isHoverBool
-        @return None
+        If user is looking at the tile
+        Switch the values of the isHoverBool
         """
         if not self.isHoverbool:
             return False
@@ -130,10 +114,8 @@ class Tiles:
 
     def recoverColor(self):
         """
-        @pre After refilling and redrawing, the color is back using this function
-        @param
-        @post When the color breaks from refilling, change the color back to the desired color
-        @return None
+        After refilling and redrawing, the color is back using this function
+        When the color breaks from refilling, change the color back to the desired color
         """
         self.org_color=(50,205,50)
         self.isHoverbool = False
